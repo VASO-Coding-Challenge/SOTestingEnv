cd3770fbdc9cb1df224f26d5ca308d24055adbb5
Andrew Lockard <alockard@unc.edu>
2024-10-04 15:58:22 -0400

    Create Vite Project

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 """Entry of the backend for the SOTesting Environment. Sets up FastAPI and SQLModel"""
 
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
-app = FastAPI()
+app = FastAPI(
+    docs_url=f"/docs",
+    openapi_url="/url_root/openapi.json",
+    redoc_url=None)
 
 @app.get("/api/hello")
 def hello_world():
