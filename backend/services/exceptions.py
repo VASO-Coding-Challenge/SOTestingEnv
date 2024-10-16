6df73eebb21f20d73cd7e131aacae3734aa78f37
Andrew Lockard <alockard@unc.edu>
2024-10-16 16:18:47 -0400

    Added UserPermissionException and polars to requirements

--- backend/services/exceptions.py
+++ backend/services/exceptions.py
@@ -4,3 +4,6 @@ class ResourceNotFoundException(Exception):
     """Raised when a user attempts to access a resource that does not exist"""
     ...
 
+class UserPermissionException(Exception):
+    """Raised when a logged in user attempts to access/modify a resource thats not allowed"""
+    ...
