4b3e5da35f9f0e64021d85083343d6a4c1fc7000
Andrew Lockard <alockard@unc.edu>
2024-10-23 12:00:54 -0400

    Fixed reset_database script

--- backend/test/fake_data/team.py
+++ backend/test/fake_data/team.py
@@ -3,7 +3,7 @@ from datetime import time, timedelta
 
 from backend.models.team import Team
 from backend.services.auth import AuthService
-from backend.services.team import TeamService
+from backend.services.passwords import PasswordService
 from backend.db import engine
 
 __authors__ = ["Mustafa Aljumayli"]
@@ -12,13 +12,10 @@ __authors__ = ["Mustafa Aljumayli"]
 def create_fake_teams(session: Session):
     """Adds the fake team data for testing purposes with hashed passwords."""
 
-    auth_service = AuthService(session)
-    team_service = TeamService(session)
-
     team1 = Team(
         id=1,
         name="B1",
-        password=team_service.generate_password(),
+        password=PasswordService.generate_password(),
         start_time=time(9, 0),
         end_time=time(9, 0),
         active_JWT=False,
@@ -26,7 +23,7 @@ def create_fake_teams(session: Session):
     team2 = Team(
         id=2,
         name="B2",
-        password=team_service.generate_password(),
+        password=PasswordService.generate_password(),
         start_time=time(9, 0),
         end_time=time(9, 0),
         active_JWT=False,
@@ -34,7 +31,7 @@ def create_fake_teams(session: Session):
     team3 = Team(
         id=3,
         name="B3",
-        password=team_service.generate_password(),
+        password=PasswordService.generate_password(),
         start_time=time(9, 0),
         end_time=time(9, 0),
         active_JWT=False,
