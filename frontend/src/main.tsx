c5da56f0796bd9678b5891d22f36428661214839
ItIsAndrewL <andrew.lockard15@gmail.com>
2024-10-31 00:54:11 -0400

    Cleaned up App Component
    This was an unnecessary compoent so I just removed it.
    
    Also configured vite config to allow for automatic browser refresh.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Question from "./pages/Question";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/question" element={<Question />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
 
