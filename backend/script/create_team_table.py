981447a47f78f4c243a947aa36508cacf73a59f7
Nick <nalmy@email.unc.edu>
2024-11-02 14:22:57 -0400

    Update ES Documentation

--- backend/script/create_team_table.py
+++ backend/script/create_team_table.py
@@ -69,7 +69,7 @@ def main():
         team_svc = TeamService(session)
         pwd_svc = PasswordService(session)
         team_list = team_svc.get_all_teams()
-        team_list = team_list + team_svc.df_to_teams(team_table)
+        # team_list = team_list + team_svc.df_to_teams(team_table)
         # Find the last team number for the prefix
         last_team = 0
         for team in team_list:
