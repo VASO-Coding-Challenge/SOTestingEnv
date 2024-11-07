bfbebe4e9b725ee54af20c8c98931eb79c8f7e06
Nick <nalmy@email.unc.edu>
2024-11-07 10:37:57 -0500

    add Reset Teams argparse

--- backend/script/reset_teams.py
+++ backend/script/reset_teams.py
@@ -4,8 +4,13 @@ from sqlmodel import Session
 from ..services import TeamService
 from ..db import engine
 import polars as pl
+import argparse
 
 __authors__ = ["Nicholas Almy"]
+
+parser = argparse.ArgumentParser(
+    description="Reset Teams: This script resets the team table and default teams file in the database. WARNING!! NOT REVERSIBLE!",
+).parse_args()
 
 with Session(engine) as session:
     team_svc = TeamService(session)
