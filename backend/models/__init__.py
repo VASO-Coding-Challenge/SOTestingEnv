f71dae20ecb67b71ac0a2082088f2cb695d5e5c7
NickA02 <nalmy@email.unc.edu>
2024-11-13 13:38:35 -0500

    Remove count from models init

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -5,9 +5,8 @@ Treat models as a package: https://docs.python.org/3/tutorial/modules.html#packa
 
 # * Note: Add all module names to this list and import for easier imports outside this module
 
-__all__ = ["count", "team", "word", "team_members", "question"]
+__all__ = ["team", "word", "team_members", "question"]
 
-from .count import Count
 from .team import Team, TeamData
 from .team_members import TeamMember, TeamMemberCreate, TeamMemberPublic
 from .word import Word
