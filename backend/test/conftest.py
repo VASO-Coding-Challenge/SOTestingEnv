3fe6eccb1a6fd3e3f81da13ef6e9935fecd2c342
Andrew Lockard <alockard@unc.edu>
2024-11-25 14:01:47 -0500

    Add testing database fixtures

---
+++ backend/test/conftest.py
@@ -0,0 +1,28 @@
+"""Test configuration file for pytest, sets up database fixtures"""
+
+import pytest
+import os
+
+from sqlmodel import create_engine, Session, SQLModel
+
+SQLITE_DATABASE_NAME = "test_database.db"
+SQLITE_DATABASE_URL = f"sqlite:///backend/test/{SQLITE_DATABASE_NAME}"
+
+__authors__ = ["Andrew Lockard"]
+
+
+@pytest.fixture(scope="session")
+def test_engine():
+    return create_engine(SQLITE_DATABASE_URL)
+
+
+@pytest.fixture(scope="function")
+def session(test_engine):
+    """Resets database tables and return a new session object"""
+    SQLModel.metadata.drop_all()
+    SQLModel.metadata.create_all()
+    session = Session(test_engine)
+    try:
+        yield session
+    finally:
+        session.close()
