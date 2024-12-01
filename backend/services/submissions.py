77e378e41e6499f9e1f69401203bbd94dff78b01
Andrew Lockard <alockard@unc.edu>
2024-12-01 17:35:41 -0500

    Add all files required for grading to zip file.
    This is needed to pass multiple files to Judge0

--- backend/services/submissions.py
+++ backend/services/submissions.py
@@ -4,9 +4,14 @@ import os
 import sys
 import requests  # type: ignore
 from ..models import Submission, ConsoleLog, Team
+from io import BytesIO  # Creates an in-memory "file"
+from zipfile import ZipFile
+from exceptions import ResourceNotFoundException
 
 
-__authors__ = ["Nicholas Almy"]
+__authors__ = ["Nicholas Almy", "Andrew Lockard"]
+
+submissions_dir = "es_files/submissions"
 
 
 class SubmissionService:
@@ -17,8 +22,7 @@ class SubmissionService:
 
     def submit(self, team_name: str, submission: Submission) -> None:
         """Submit a file to the submission folder... Only supports Python files"""
-        sys.stdout.write(f"Debug: Team name is {team_name}")
-        submissions_dir = "es_files/submissions"
+        # sys.stdout.write(f"Debug: Team name is {team_name}")
         question_dir = f"q{submission.question_num}"
         file = f"{team_name}.py"
 
@@ -77,3 +81,53 @@ class SubmissionService:
             return ConsoleLog(console_log=actual_response)
         else:
             return ConsoleLog(console_log=f"Error: {res.json()["error"]} Submission")
+
+    def package_submission(
+        self, team_name: str, question_number: int, demo=False
+    ) -> str:
+        """Packages submission files into a string of a .zip contents.
+
+        This packages together all files in autograder_utils
+        as well as the `test_cases.py` (if demo = False) file for that question.
+        With demo=True, `demo_cases.py` is packaged instead.
+        """
+
+        utils_dir = "backend/autograder_utils"
+        question_dir = os.path.join("es_files", "questions", f"q{question_number}")
+
+        # with BytesIO() as f: # Creates an in memory buffer we can use just like a file
+        with ZipFile(
+            "new_zip.zip", "w"
+        ) as new_zip:  # Creates a new zip in memory we can add to
+            # Add all files in autograder_utils
+            if not os.path.exists(utils_dir):
+                raise ResourceNotFoundException("No Utils Created.")
+
+            for file in os.listdir(utils_dir):
+                new_zip.write(os.path.join(utils_dir, file), arcname=file)
+
+            # Add test/demo case file
+            if demo:
+                path = os.path.join(question_dir, "demo_cases.py")
+                if not os.path.exists(path):
+                    raise ResourceNotFoundException(
+                        f"Question {question_number} not found"
+                    )
+                new_zip.write(path, arcname="demo_cases.py")
+            else:
+                path = os.path.join(question_dir, "test_cases.py")
+                if not os.path.exists(path):
+                    raise ResourceNotFoundException(
+                        f"Question {question_number} not found"
+                    )
+                new_zip.write(path, arcname="test_cases.py")
+
+            # Add submission file
+            path = os.path.join(
+                submissions_dir, f"q{question_number}", f"{team_name}.py"
+            )
+            if not os.path.exists(path):
+                raise ResourceNotFoundException(
+                    f"Team {team_name} did not submit question {question_number}"
+                )
+            new_zip.write(path, arcname="submission.py")
