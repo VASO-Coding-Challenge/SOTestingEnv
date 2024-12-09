e66b7448df1c0c4812a52f3f9d962b7f69385d99
Matthew Futch <111521614+mattfutch01@users.noreply.github.com>
2024-12-08 20:53:56 -0500

    More tests for question; 100% coverage (#76)
    Last few question tests to get the coverage to 100%

--- backend/test/question_test.py
+++ backend/test/question_test.py
@@ -56,3 +56,42 @@ def test_refresh_questions(question_svc, setup_questions):
     refreshedQuestions = question_svc.load_questions()
 
     assert startingQuestions.questions == refreshedQuestions.questions
+
+
+def test_load_questions_file_not_found_error(question_svc, setup_questions):
+    tmp_path = setup_questions(2)
+    os.remove(tmp_path / "es_files" / "questions" / "q1" / "prompt.md")
+    os.chdir(tmp_path)
+
+    try:
+        question_svc.load_questions()
+    except FileNotFoundError:
+        pytest.fail("FileNotFoundError was not handled correctly")
+
+
+def test_load_questions_file_fails_not_questionDir(question_svc, setup_questions):
+    tmp_path = setup_questions(2)
+    os.mkdir(tmp_path / "es_files" / "questions" / "question1")
+    os.chdir(tmp_path)
+
+    assert question_svc.load_questions()
+
+
+def test_load_question_with_no_starter_code(question_svc, setup_questions):
+    tmp_path = setup_questions(2)
+    os.remove(tmp_path / "es_files" / "questions" / "q1" / "starter.py")
+    os.chdir(tmp_path)
+    assert (question_svc.load_question(1).starter_code) == ""
+
+
+def test_load_questions_generic_exception(question_svc, setup_bad_questions):
+    # Create a test environment that triggers a generic exception
+    tmp_path = setup_bad_questions(2)
+    os.chmod(
+        tmp_path / "es_files" / "questions" / "q1" / "prompt.md", 0o000
+    )  # Restrict permissions to cause an exception
+    os.chdir(tmp_path)
+
+    # Ensure the generic exception is raised correctly
+    with pytest.raises(Exception):
+        question_svc.load_questions()
