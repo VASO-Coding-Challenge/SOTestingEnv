f0cdfefbad0b47f7e8d653376598116bab2978db
Ivan Wu <ivanwu@unc.edu>
2025-02-13 14:10:49 -0500

    Fix db.py bug

--- backend/db.py
+++ backend/db.py
@@ -1,16 +1,11 @@
 """Creates the SQLModel Engine to be used across the application."""
 
 from sqlmodel import create_engine, Session
-import os
 
 __authors__ = ["Andrew Lockard, Ivan Wu"]
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///backend/{sqlite_file_name}"
-
-old_db_path = "backend/database.db"
-if os.path.exists(old_db_path):
-    os.remove(old_db_path)
 
 
 # TODO: Create script to turn off echo when NOT in development mode
