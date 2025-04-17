e0adca1104430ce8bf8cb99317de97e0d4e8540f
Michelle Nguyen <michelleclt9925@gmail.com>
2025-04-17 12:40:44 -0400

    update actions yml: change to backend config file so that the env file is correct

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,13 @@ import os
 
 __authors__ = ["Mustafa Aljumayli"]
 
-load_dotenv(os.path.join(os.getcwd(), "backend/.env.development"))
+# load_dotenv(os.path.join(os.getcwd(), "backend/.env.development"))
+
+# Get the path of the .env file relative to this config.py file
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_PATH = os.path.join(BASE_DIR, ".env.development")
+
+load_dotenv(ENV_PATH)
 
 # Access the environment variables
 SECRET_KEY = os.getenv("SECRET_KEY")
